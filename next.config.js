fcf74bea084d08ebf28b9540ab09ab4fdef92d28
saeed <saeedblack77@gmail.com>
2023-04-09 23:20:59 +0330

    khooneh 1402/01/20 23:20

--- next.config.js
+++ next.config.js
@@ -8,12 +8,13 @@ if (!process.env.WORDPRESS_API_URL) {
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: [
-      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
-      '0.gravatar.com',
-      '1.gravatar.com',
-      '2.gravatar.com',
-      'secure.gravatar.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'http://localhost/test.raiatec.com/',
+        port: '',
+        pathname: '/uploads/**',
+      },
     ],
   },
 }
